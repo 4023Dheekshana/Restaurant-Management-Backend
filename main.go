5f95cb557f88efe562e949619a779321e6e775f5
Dheekshana Sivaselvam <Dheekshana@tsi.com>
2024-11-14 02:34:18 -0800

    Program starts here

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"golang-restaurant-management/middleware"
+	"golang-restaurant-management/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+//var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.OrderRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderItemsRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+}
